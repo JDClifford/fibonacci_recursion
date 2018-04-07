57da34bda64afc61422bb404d44b8406a9853f77
John <johnclifford00@gmail.com>
2018-04-07 13:11:59 -0400

    Added recursive method for solving fibonacci sequence

--- lib/fib.rb
+++ lib/fib.rb
@@ -19,8 +19,22 @@ def fibs(num)
 		x += 1
 	end
 end
-fibs(10)
 
+
+$num1 = 1
+$num2 = 1
 def fibs_rec(num)
-
+	unless num == 0 
+		if $num1 < $num2 
+			puts $num1 
+			$num1 += $num2 
+		else
+			puts $num2 
+			$num2 += $num1
+		end
+	num -= 1
+	fibs_rec(num)
+	end
 end
+
+fibs_rec(10)
